43dbb45ef36b216addc6e885ebe6797df78fabca
Animeshpatra2001 <147701997+Animeshpatra2001@users.noreply.github.com>
2024-11-26 16:24:09 +0530

    Add files via upload

---
+++ Assignment1/Q05b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int x = -123;
+    printf("%X", x);
+    return (0);
+}
+
+/* 
+%X is unsigned integer in hexadecimal format specifier, 
+hence it will return the 32bit 2's complement of -123 in hexadecimal format.
+*/
+
+/*
+output
+FFFFFF85
+*/
